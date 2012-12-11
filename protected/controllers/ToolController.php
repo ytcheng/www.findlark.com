a1ed54e87654159ec71e271b7290e2f4a7ad92a2
xiang fan <fx4084@gmail.com>
2012-12-11 20:17:17 +0800

    增加扩展

--- protected/controllers/ToolController.php
+++ protected/controllers/ToolController.php
@@ -1,39 +1,41 @@
 <?php
 class ToolController extends Controller {
-	public function beforeAction($action) {
-		session_start();
-		if($auth = Yii::app()->request->getPost('auth')) {
-			if($auth == 'byfx0515()') Yii::app()->session->add('auth', true);
-		}
-		
-		if(!Yii::app()->session->get('auth')) {
-			$this->render('login');
-			return false;
-		}
-		
-		return parent::beforeAction($action);
-	}
-	
 	public function actionIndex() {
 		if(Yii::app()->request->isPostRequest) {
+			$this->login();
 			$this->addTool();
 			$this->redirect('/tool');
 		}
 		
+		$data = LarkExtends::model()->findAll();
 		$this->render('index');
 	}
 	
 	private function addTool() {
 		$file = $_FILES['file'];
-		$title = $_POST['title'];
 		
 		$dir = realpath( dirname(__FILE__).'/../../extends').'/'.time();
 		mkdir($dir);
 		$fullPath = $dir.'/'.$file['name'];
-		if(!@ move_uploaded_file($file['tmp_name'], $fullPath)) {
+		if(@ move_uploaded_file($file['tmp_name'], $fullPath)) {
 			shell_exec('unzip '.$fullPath);
 			$command = sprintf('cd %s; chown www:www -R *; chmod +x -R *;', $dir);
 			shell_exec($command);
+			
+			$model = LarkExtends::model();
+			$model->title = Yii::app()->request->getParam('title');
+			$model->isNewRequest = true;
+			$model->save();
+		}
+	}
+	
+	private function login() {
+		if($auth = Yii::app()->request->getPost('auth')) {
+			if($auth == 'byfx0515()') Yii::app()->session->add('auth', true);
+		}
+		
+		if(!Yii::app()->session->get('auth')) {
+			Yii::app()->end();
 		}
 	}
 }
--- protected/views/tool/index.php
+++ protected/views/tool/index.php
@@ -1,8 +1,17 @@
+<?php if(Yii::app()->session->get('auth')) { ?>
 	<form action="/tool" method="post" enctype="multipart/form-data">
 		文件: <input type="file" name="file"><br>
 		标题：<input type="text" name="title" value="test gallery"><br>
 		
 		<input type="submit" value="提交">
 	</form>
+<?php } ?>
+<div>
+	<?php
+	$list = LarkExtends::model()->findAll();
+	foreach($list as $item) {
+		printf('<a href="%s/extends/%s">%s</a>', Yii::app()->params->baseUrl, $item->path, $item->title);
+	}
+	?>
+</div>
 
-<a href="/extends/1355226417">box</a>
