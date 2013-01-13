72dec45a64b298890ceb72a31cc1279b75a5187a
fan xiang <664712890@qq.com>
2013-01-13 21:00:18 +0800

    资源

--- source/table/lark_mark.sql
+++ source/table/lark_mark.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 01 月 13 日 05:45
+-- 生成日期: 2013 年 01 月 13 日 12:59
 -- 服务器版本: 5.5.27
 -- PHP 版本: 5.3.19
 
@@ -28,13 +28,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `lark_mark` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `content` text,
-  `latitude` float NOT NULL,
-  `longitude` float NOT NULL,
+  `title` varchar(100) NOT NULL COMMENT '标题',
+  `content` text COMMENT '内容',
+  `latitude` float NOT NULL COMMENT '纬度',
+  `longitude` float NOT NULL COMMENT '经度',
   `display` tinyint(4) NOT NULL DEFAULT '1',
-  `timeline` int(10) NOT NULL,
-  `author` varchar(20) DEFAULT NULL,
+  `timeline` int(10) NOT NULL COMMENT '发表时间',
+  `author` varchar(20) DEFAULT NULL COMMENT '作者',
+  `icon` varchar(10) DEFAULT 'say' COMMENT '标记图标',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
