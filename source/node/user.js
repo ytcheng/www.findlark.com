d97af1d59a844e41c564b8e891d4dc8c3e545c5b
fan xiang <664712890@qq.com>
2013-01-15 23:53:21 +0800

    chat

--- source/node/user.js
+++ source/node/user.js
@@ -1,20 +1,37 @@
 //var io = require('socket.io').listen(9092, '64.34.253.111'),
 var io = require('socket.io').listen(80),
+		cookie = require('cookie'),
 		redis = require("redis"),
 		client = redis.createClient(6379, '127.0.0.1'),
-		http = require('http'),
 		users = {};
 
-io.of('/private').authorization(function (handshakeData, callback) {
-  console.dir(handshakeData);
-  handshakeData.foo = 'baz';
-  callback(null, true);
-}).on('connection', function (socket) {
-	socketList[socket.id] = socket;
-	console.log("connect:"+socket.id);
-
+io.of("/user").authorization(function(handshakeData, callback) {
+	if(!handshakeData.headers.cookie) {
+		return callback("cookie is null!", false);
+	}
+	
+	var cookies = cookie.parse(handshakeData.headers.cookie);
+	if(!cookies.uid) {
+		return callback("Login status error!", false);
+	}
+	
+	handshakeData.cookies = cookies;
+	callback(null, true);
+}).on('connection', function(socket) {
+	//console.log(socket.handshake.cookies);
+	var uid = socket.handshake.cookies.uid;
+	users[uid] = socket;
+	
+	console.log(uid);
+	
 	socket.on('disconnect', function () {
-		delete socketList[socket.id];
-		console.log("disconnect:"+socket.id);
+		delete users[uid];
+		console.log("disconnect:"+uid);
+	});
+	
+	socket.on("chat", function(data) {
+		console.log(data);
+		users[data.toUid].emit("news", {time:"12121", title:data.msg});
 	});
 });
+
