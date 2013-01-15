53c72973e475f9c4f67f90a14a8decb069eaa589
xiang fan <fx4084@gmail.com>
2013-01-15 17:47:35 +0800

    sql

--- source/table/findlark.sql
+++ source/table/findlark.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 01 月 15 日 05:00
+-- 生成日期: 2013 年 01 月 15 日 09:47
 -- 服务器版本: 5.5.20
 -- PHP 版本: 5.2.9-1
 
@@ -33,6 +33,14 @@ CREATE TABLE IF NOT EXISTS `lark_extends` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='扩展' AUTO_INCREMENT=4 ;
 
+--
+-- 转存表中的数据 `lark_extends`
+--
+
+INSERT INTO `lark_extends` (`id`, `title`, `path`) VALUES
+(1, 'JQuery插件-mini提示框', '1355226417'),
+(3, 'HTML5 绘制图片直方图', 'histogram');
+
 -- --------------------------------------------------------
 
 --
@@ -42,10 +50,25 @@ CREATE TABLE IF NOT EXISTS `lark_extends` (
 CREATE TABLE IF NOT EXISTS `lark_friend` (
   `uid` int(11) NOT NULL,
   `f_uid` int(11) NOT NULL,
-  `timeline` int(10) NOT NULL,
+  `timeline` int(10) NOT NULL COMMENT '成为好友时间',
   `remark` varchar(20) DEFAULT NULL COMMENT '备注',
+  `group_id` int(11) DEFAULT '0' COMMENT '分组ID',
   PRIMARY KEY (`uid`,`f_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='好友关系表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `lark_friend_group`
+--
+
+CREATE TABLE IF NOT EXISTS `lark_friend_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL COMMENT '组名称',
+  `uid` int(11) NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户对好友的分组' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -60,6 +83,15 @@ CREATE TABLE IF NOT EXISTS `lark_image` (
   `title` varchar(200) DEFAULT '' COMMENT '图片标题',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='图片' AUTO_INCREMENT=13 ;
+
+--
+-- 转存表中的数据 `lark_image`
+--
+
+INSERT INTO `lark_image` (`id`, `panoramio_id`, `src`, `title`) VALUES
+(10, 84493955, '/upload/2013/84493955/QXOOWHN.jpg', ''),
+(11, 84493955, '/upload/2013/84493955/QXOOWHO.jpg', ''),
+(12, 84493955, '/upload/2013/84493955/QXOOWHQ.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -83,6 +115,53 @@ CREATE TABLE IF NOT EXISTS `lark_mark` (
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `lark_novel`
+--
+
+CREATE TABLE IF NOT EXISTS `lark_novel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `summary` varchar(300) NOT NULL COMMENT '简介',
+  `content` text NOT NULL COMMENT '内容',
+  `display` tinyint(1) DEFAULT '0' COMMENT '是否显示 0 否 1 是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `lark_picture`
+--
+
+CREATE TABLE IF NOT EXISTS `lark_picture` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `title` varchar(100) NOT NULL COMMENT '图片标题',
+  `classify` smallint(6) DEFAULT '0' COMMENT '分类，0 未分类',
+  `tag` varchar(100) DEFAULT NULL COMMENT '标签',
+  `desc` varchar(200) DEFAULT NULL COMMENT '描述',
+  `timeline` int(10) NOT NULL COMMENT '添加时间',
+  `width` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '图片实际宽度',
+  `height` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '图片实际高度',
+  `dir` varchar(200) NOT NULL COMMENT '图片保存路径',
+  `name` char(8) NOT NULL,
+  `ext` char(5) NOT NULL COMMENT '图片扩展名',
+  `histogram` text COMMENT '直方图信息',
+  `properties` text COMMENT '图片属性',
+  `hash` char(16) NOT NULL COMMENT '图片hash值，16进制保存',
+  `share_times` int(10) unsigned DEFAULT '0' COMMENT '被分享次数',
+  `score` int(10) unsigned DEFAULT '0' COMMENT '图片评分',
+  `display` tinyint(4) DEFAULT '0' COMMENT '是否显示， 0 否 1是',
+  `original_url` varchar(200) DEFAULT NULL COMMENT '图片源地址',
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`),
+  KEY `tag` (`tag`),
+  KEY `hash` (`hash`),
+  KEY `ext` (`ext`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='图片' AUTO_INCREMENT=184 ;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `lark_user`
 --
 
@@ -97,6 +176,13 @@ CREATE TABLE IF NOT EXISTS `lark_user` (
   PRIMARY KEY (`uid`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `lark_user`
+--
+
+INSERT INTO `lark_user` (`uid`, `nickname`, `email`, `password`, `active`, `jointime`, `avatar`) VALUES
+(3, '老陌', '123@qq.com', 'f020e337d84833cd2e4cbdaa367b64dc', 1, 1358221044, '');
 
 -- --------------------------------------------------------
 
@@ -124,6 +210,19 @@ CREATE TABLE IF NOT EXISTS `lark_user_log` (
   `login_failed_time` tinyint(4) DEFAULT '0' COMMENT '登录失败次数',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户日志';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `urls`
+--
+
+CREATE TABLE IF NOT EXISTS `urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `url` varchar(200) NOT NULL,
+  `hash` char(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
